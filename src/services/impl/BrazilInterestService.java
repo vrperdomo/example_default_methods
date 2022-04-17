fba4c8304b89b1d08d5a3df93091777a0af6fb58
victor <victor.rodriguesp@gmail.com>
2022-04-17 14:35:57 -0300

    Examplo de default methods

---
+++ src/services/impl/BrazilInterestService.java
@@ -0,0 +1,18 @@
+package services.impl;
+
+import services.InterestService;
+
+public class BrazilInterestService implements InterestService {
+
+    private double interestRate;
+
+    public BrazilInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+}
